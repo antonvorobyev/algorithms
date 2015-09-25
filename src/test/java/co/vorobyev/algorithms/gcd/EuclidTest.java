b5e54218c3e6ec50784248036d5946a215d0ced6
Anton Vorobyev <anton@vorobyev.co>
2015-09-25 13:14:03 +0500

    Add unit test case for euclidian gcd

---
+++ src/test/java/co/vorobyev/algorithms/gcd/EuclidTest.java
@@ -0,0 +1,16 @@
+package co.vorobyev.algorithms.gcd;
+
+import static co.vorobyev.algorithms.gcd.Euclid.gcd;
+
+import org.junit.Test;
+
+public class EuclidTest {
+
+    @Test
+    public void test() {
+    }
+
+
+
+
+}
